03280703cfd48999fe34e42b69ef7d2323a7d875
policypal1 <samuelkumpula175@gmail.com>
2026-03-10 05:52:00 -0700

    Revert "Add Vercel Speed Insights integration guide"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }) {
   return (
@@ -7,7 +6,6 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
